b7ad0e48dd24bfc6d5baed2f7ebc34308443ee87
leeyell <lylim5050@kookmin.ac.kr>
2020-11-08 22:23:24 +0900

    correct 세는 부분 수정

--- train.py
+++ train.py
@@ -24,12 +24,12 @@ from data_loader import TimeStampDataset
 ############### 하이퍼 파라미터
 nEpochs = 100       # Number of epochs for training
 snapshot = 3       # Store a model every snapshot epochs
-lr = 1e-3
+lr = 0.03
 batch_size = 10
-threshold = 0.01        # output 의 확률과 label 의 확률이 이 값 미만이면 correct += 1 해주는 임계값...
+threshold = 0.2        # 클래스 0 ~ 3 중 하나를 예측하는 가장 높은 확률이 이 값 미만이면 해당 예측값은 해당되는 class 가 없다는, 즉 클래스 4 를 예측하는 것으로 친다.
 
 
-save_path = "./save"
+save_path = "./save/1108_22_20"
 dataset_path = "D:/news_frame"
 xlsx_path = "./video_labeling.xlsx"
 log_dir = os.path.join(save_path, 'logs')
@@ -37,6 +37,10 @@ log_dir = os.path.join(save_path, 'logs')
 
 
 def train():
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("Device being used:", device)
 
@@ -56,7 +60,7 @@ def train():
     # Optimizer
     optimizer = optim.Adam(net.parameters(), lr=lr, weight_decay=5e-4)
     # Scheduler
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)  # 20 에폭마다 learning rate 0.5배
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.5)  # 20 에폭마다 learning rate 0.5배
 
 
     net.to(device)
@@ -94,23 +98,49 @@ def train():
         for inputs, labels in tqdm(train_loader):
             inputs = Variable(inputs, requires_grad=True).to(device=device, dtype=torch.float32)
             labels = Variable(labels).to(device=device, dtype=torch.float32)
-            # labels 를 1차원으로 펴준다. --> [0 ~ n 프레임의 클래스 0 일 확률 쭉 ... ,
-            #                                         0 ~ n 프레임의 클래스 1 일 확률 쭉 ... ,
-            #                                               0 ~ n 프레임의 클래스 2 일 확률 쭉 ..., ...]
-            labels = labels.view(labels.shape[0], -1)    # shape = (batch_size, 4*frames)
-
+            #### labels 를 1차원으로 펴준다. --> [0 ~ n 프레임의 클래스 0 일 확률 쭉 ... ,
+            ####                                         0 ~ n 프레임의 클래스 1 일 확률 쭉 ... ,
+            ####                                               0 ~ n 프레임의 클래스 2 일 확률 쭉 ..., ...]
+            #### labels = labels.view(labels.shape[0], -1)    # shape = (batch_size, 4*frames)
             
             optimizer.zero_grad()
 
             outputs = net(inputs)
-            probs = sigmoid(outputs)
-            loss = criterion(probs, labels)
 
+            # outputs 은 지금 이 꼴로 나옴 --> 배치 개수 x [0 ~ n 프레임의 클래스 0 일 확률 쭉 ... ,
+            #                                                   0 ~ n 프레임의 클래스 1 일 확률 쭉 ... ,
+            #                                                           0 ~ n 프레임의 클래스 2 일 확률 쭉 ..., ...]    --> (Batch, Class*Frame)
+            outputs = outputs.reshape(labels.shape[0], labels.shape[1], labels.shape[2])        # labels 모양이랑 맞춰주고 --> (Batch, Class, Frame)
+            probs = sigmoid(outputs)
+
+
+
+            ################## Loss 측정
+            loss = criterion(probs, labels)
+            running_loss += loss.item() * inputs.size(0)
+
+
+            ################## Accuracy 측정
+            # labels 에서 각 클래스 별 가장 높은 확률들만 가져옴
+            # [class 0 을 나타내는 확률 중 가장 높은 값, class 1 을 나타내는 확률 중 가장 높은 값, ... ] --> (10, 4)
+            labels = torch.max(labels, dim=2)[0]        # 여기서 labels 값은 각 클래스 별 가장 높은 확률임
+            # 그 중에서도 제일 높은 확률을 갖는 class 가 해당 프레임 시퀀스의 class 로 결정   --> (10)
+            labels = torch.max(labels, dim=1)[1]        # 여기서 labels 값은 클래스 그 자체임
+
+            # outputs 에서 각 클래스 별 가장 높은 확률들만 가져옴
+            # [class 0 을 나타내는 확률 중 가장 높은 값, class 1 을 나타내는 확률 중 가장 높은 값, ... ] --> (10, 4)
+            outputs = torch.max(probs, dim=2)[0]
+            # 어떤 class 인지는 몰라도... 가장 높은 확률값인데? threshold 이상의 확률이 아니라면 아무 클래스도 아니라는... 4 의 class 를 부여
+            # threshold 를 넘는 확률값이라면 그 class 를 가져옴 (argmax = 클래스니까 인덱스를 가져와야 함)
+            outputs = torch.tensor([output.argmax() if output.max() > threshold else 4 for output in outputs])      # --> (10)
+            
+            outputs = outputs.to(device=device, dtype=torch.float32)
+            running_corrects += torch.sum(outputs == labels)
+
+
+            
             loss.backward()
             optimizer.step()
-
-            running_loss += loss.item() * inputs.size(0)
-            running_corrects += torch.sum(abs(probs - labels.data) < threshold)
 
 
         epoch_loss = running_loss / len(train_loader.dataset)
@@ -169,7 +199,7 @@ def train():
                 'state_dict': net.state_dict(),
                 'opt_dict': optimizer.state_dict(),
             }, os.path.join(save_path, 'test_epoch-' + str(epoch) + '-best_model.pth'))
-            best_acc_on_test = apoch_acc
+            best_acc_on_test = epoch_acc
 
         print("[test] Epoch: {}/{} Loss: {} Acc: {}".format(epoch+1, nEpochs, epoch_loss, epoch_acc))
         stop_time = timeit.default_timer()
