53b58783aaee94c8955e022f4701788b4f769761
cokwa <rhrh1363@gmail.com>
2020-11-10 21:44:40 +0900

    자료화면 재추가

--- range_data_loader.py
+++ range_data_loader.py
@@ -27,48 +27,47 @@ class RangeDataset(Dataset):
 
     wb = xlrd.open_workbook(xlsx_path)
     sheet = wb.sheet_by_index(0)
-    headers = [sheet.cell_value(0, col) for col in range(5)]
+    headers = [sheet.cell_value(0, col).lower() for col in range(5)]
     
     self.labels = []
 
     row = 1
-    while True:
-      row_labels = {}
-      for col in range(5):
-        try:
-          row_labels[headers[col]] = sheet.cell_value(row, col)
-        except:
+    while True: 
+      try:
+        if sheet.cell_value(row, 0) == '':
           break
-      else:
-        self.labels.append(row_labels)
-        row += 1
-        continue
-      break
-
+      except:
+        break
+      row_labels = {}     
+      for col in range(5):
+        row_labels[headers[col]] = sheet.cell_value(row, col)
+      self.labels.append(row_labels)
+      row += 1
+      
     self.total_frame_count = 0
     self.total_data_count = 0
 
     for row_labels in self.labels:
-      row_labels['Video_Id'] = int(row_labels['Video_Id']) if row_labels['Video_Id'] != '' else -1
+      row_labels['video_id'] = int(row_labels['video_id']) if row_labels['video_id'] != '' else -1
 
-      time_stamps = filter(lambda x: x != '', row_labels['Time_stamp'].split(','))
+      time_stamps = filter(lambda x: x != '', row_labels['time_stamp'].split(','))
       time_stamps_split = map(lambda x: x.split(':'), time_stamps)
-      row_labels['Time_stamp'] = list(map(lambda x: int(x[0]) * 60 + int(x[1]), time_stamps_split))
+      row_labels['time_stamp'] = list(map(lambda x: int(x[0]) * 60 + int(x[1]), time_stamps_split))
       row_labels['time_stamp_class'] = list(map(int, filter(lambda x: x != '', row_labels['time_stamp_class'].split(','))))
 
-      index = 0
+      '''index = 0
       while index < len(row_labels['time_stamp_class']):
         if row_labels['time_stamp_class'][index] == 3:
-         del row_labels['Time_stamp'][index]
+         del row_labels['time_stamp'][index]
          del row_labels['time_stamp_class'][index]
         else:
-          index += 1
+          index += 1'''
       
-      video_name = row_labels['Video_name']
+      video_name = row_labels['video_name']
       ext = video_name.rfind('.')
       if ext > 0:
         video_name = video_name[:ext]
-      row_labels['Video_name'] = video_name
+      row_labels['video_name'] = video_name
       
       row_labels['frames'] = len([path for path in os.listdir(os.path.join(self.root_path, video_name)) if path[-4:] == '.jpg'])
       row_labels['data_count'] = int(np.ceil((row_labels['frames'] - self.frame_overlaps) / (self.frames - self.frame_overlaps)))
@@ -76,11 +75,11 @@ class RangeDataset(Dataset):
       self.total_frame_count += row_labels['frames']
       self.total_data_count += row_labels['data_count']
 
-    self.label = torch.zeros(3, self.total_frame_count)
+    self.label = torch.zeros(4, self.total_frame_count)
     
     frame_offset = 0
     for row_labels in self.labels:
-      timestamps = row_labels['Time_stamp']
+      timestamps = row_labels['time_stamp']
       ts_classes = row_labels['time_stamp_class']
       frames = row_labels['frames']
 
@@ -112,7 +111,7 @@ class RangeDataset(Dataset):
 
     images = []
     for frame in range(frame_start, frame_end):
-      images.append(self.transform(Image.open(os.path.join(self.root_path, row_labels['Video_name'], f'{str(frame + 1).zfill(5)}.jpg'))))
+      images.append(self.transform(Image.open(os.path.join(self.root_path, row_labels['video_name'], f'{str(frame + 1).zfill(5)}.jpg'))))
 
     return torch.stack(images, dim=1), self.label[:, frame_offset + frame_start:frame_offset + frame_end]
 
@@ -131,7 +130,7 @@ if __name__ == '__main__':
       for ax, img in zip(axes, dat.permute(1, 2, 3, 0)):
         ax.imshow(img)
         ax.axis('off')
-      fig, axes = plt.subplots(3, 1, figsize=(10, 3))
+      fig, axes = plt.subplots(4, 1, figsize=(10, 3))
       for ax, img in zip(axes, lbl):
         ax.imshow(img.view(1, 24), vmin=0, vmax=1)
         ax.axis('off')
